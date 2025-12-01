31379682a24073e3f97876258073e163790cd9d6
Marco Beyer <MBeyer2@gmail.com>
2025-12-01 22:36:11 +0100

    Upgrade deps

--- src/helpers/getVoteMetadata.ts
+++ src/helpers/getVoteMetadata.ts
@@ -1,5 +1,5 @@
 import { formatFixed } from "@ethersproject/bignumber";
-import { BigNumberish } from "ethers"
+import { BigNumberish } from "ethers";
 
 export const getPrecisionForIdentifier = (identifier: string): number => {
   return isNon18Precision(identifier)
@@ -34,75 +34,78 @@ function isNon18Precision(
   return identifier in IDENTIFIER_NON_18_PRECISION;
 }
 export const parseTitle = (
-    decodedAncillaryData: string,
-    titleIdentifier = "title:",
-    descriptionIdentifier = "description:"
-  ) => {
-    const start = decodedAncillaryData.indexOf(titleIdentifier);
-    const end =
-      decodedAncillaryData.indexOf(descriptionIdentifier) ??
-      decodedAncillaryData.length;
+  decodedAncillaryData: string,
+  titleIdentifier = "title:",
+  descriptionIdentifier = "description:"
+) => {
+  const start = decodedAncillaryData.indexOf(titleIdentifier);
+  const end =
+    decodedAncillaryData.indexOf(descriptionIdentifier) ??
+    decodedAncillaryData.length;
 
-    if (start === -1) {
-      return decodedAncillaryData;
-    }
-
-    const title = decodedAncillaryData
-      .substring(start + titleIdentifier.length, end)
-      .trim();
-    // remove the trailing comma if it exists (from Polymarket)
-    return title.endsWith(",") ? title.slice(0, -1) : title;
-  };
-
-export const parseDescription = (
-    decodedAncillaryData: string,
-    descriptionIdentifier = "description:"
-  ) => {
-    if (!decodedAncillaryData) {
-      return undefined;
-    }
-    const start = decodedAncillaryData.indexOf(descriptionIdentifier);
-    const end = decodedAncillaryData.length;
-
-    if (start === -1) {
-      return undefined;
-    }
-
-    return decodedAncillaryData.substring(
-      start + descriptionIdentifier.length,
-      end
-    );
-  };
-
-  export const formatVoteStringWithPrecision = (
-    vote: BigNumberish,
-    decodedIdentifier: string
-  ) => {
-    // check the precision to use from our table of precisions
-    const identifierPrecision = BigInt(
-      getPrecisionForIdentifier(decodedIdentifier)
-    );
-    const formatted = formatFixed(vote, identifierPrecision).toString();
-    // if the formatted number ends with .0, remove the .0
-    return formatted.endsWith(".0") ? formatted.slice(0, -2) : formatted;
+  if (start === -1) {
+    return decodedAncillaryData;
   }
 
-  export const formatVoteString = (
-    vote: BigNumberish,
-    decodedIdentifier: string
-  ) => {
-    const formatted = formatVoteStringWithPrecision(vote, decodedIdentifier);
-    if(decodedIdentifier === "YES_OR_NO_QUERY") {
-      if(formatted === "0") {
-        return "NO";
-      }
-      if(formatted === "1") {
-        return "YES";
-      }
-      if(formatted === "-57896044618658097711785492504343953926634992332820282019728.792003956564819968") {
-        return "TOO EARLY";
-      }
-      return formatted;
+  const title = decodedAncillaryData
+    .substring(start + titleIdentifier.length, end)
+    .trim();
+  // remove the trailing comma if it exists (from Polymarket)
+  return title.endsWith(",") ? title.slice(0, -1) : title;
+};
+
+export const parseDescription = (
+  decodedAncillaryData: string,
+  descriptionIdentifier = "description:"
+) => {
+  if (!decodedAncillaryData) {
+    return undefined;
+  }
+  const start = decodedAncillaryData.indexOf(descriptionIdentifier);
+  const end = decodedAncillaryData.length;
+
+  if (start === -1) {
+    return undefined;
+  }
+
+  return decodedAncillaryData.substring(
+    start + descriptionIdentifier.length,
+    end
+  );
+};
+
+export const formatVoteStringWithPrecision = (
+  vote: BigNumberish,
+  decodedIdentifier: string
+) => {
+  // check the precision to use from our table of precisions
+  const identifierPrecision = BigInt(
+    getPrecisionForIdentifier(decodedIdentifier)
+  );
+  const formatted = formatFixed(vote, identifierPrecision).toString();
+  // if the formatted number ends with .0, remove the .0
+  return formatted.endsWith(".0") ? formatted.slice(0, -2) : formatted;
+};
+
+export const formatVoteString = (
+  vote: BigNumberish,
+  decodedIdentifier: string
+) => {
+  const formatted = formatVoteStringWithPrecision(vote, decodedIdentifier);
+  if (decodedIdentifier === "YES_OR_NO_QUERY") {
+    if (formatted === "0") {
+      return "NO";
+    }
+    if (formatted === "1") {
+      return "YES";
+    }
+    if (
+      formatted ===
+      "-57896044618658097711785492504343953926634992332820282019728.792003956564819968"
+    ) {
+      return "TOO EARLY";
     }
     return formatted;
   }
+  return formatted;
+};
